0147c14a6f7c7cb99bfe75039513c4786ce77e11
Casper G. Nielsen <cn@helicene.com>
2022-05-28 22:15:16 +0200

    Use BigUInt to avoid overflowing

--- Sources/SwiftPaillier/Paillier.swift
+++ Sources/SwiftPaillier/Paillier.swift
@@ -201,14 +201,14 @@ public class PaillierEncryption: Encodable {
     }
     
     // This function is derived from https://github.com/CasperGN/rust-paillier/blob/master/src/arithimpl/gmpimpl.rs#L20-L26
-    private func sample(bitsize: Int) -> Int {
+    private func sample(bitsize: Int) -> BigUInt {
         let bytes = (bitsize - 1) / 8 + 1
         var buf: Array<Int> = []
         for _ in 0...bytes {
             buf.append(Int.random(in: 1..<10) >> (bytes * 8 - bitsize))
         }
         
-        return buf.reduce(0, {$0 * 10 + $1})
+        return BigUInt(buf.reduce(0, {$0 * 10 + $1}))
     }
     
     private func SampleBelow(n: BigUInt) -> BigUInt {
